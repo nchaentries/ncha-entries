e7076994513c92f722849130f807906b921279cd
Prashanth Kuchanpally <prshnth@users.noreply.github.com>
2020-07-20 22:55:16 -0400

    added db uri config var

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
+        uri: env("DATABASE_URI"),
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
         ssl: true,
       },
     },
